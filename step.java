2a340ab15485d0303bff1694cbcf32bacc932c03
kkakti2-stack <kakakikritangka2@gmail.com>
2026-03-04 02:17:14 +0530

    UC2

--- step.java
+++ step.java
@@ -1,5 +1,12 @@
 public class step {
     public static void main(String args[]) {
-        System.out.println("O O P S");
+        System.out.println("  *****     *****     *****     *****  ");
+        System.out.println(" *     *   *     *   *     *   *     * ");
+        System.out.println(" *     *   *     *   *     *   *       ");
+        System.out.println(" *     *   *     *   *  * *      *****  ");
+        System.out.println(" *     *   *     *   *               * ");
+        System.out.println(" *     *   *     *   *         *     * ");
+        System.out.println("  *****     *****    *          *****  ");
+
     }
 }
