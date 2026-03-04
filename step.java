5bc18c9fab15614942c33625ce35988384f8c73c
kkakti2-stack <kakakikritangka2@gmail.com>
2026-03-04 19:06:58 +0530

    uc4

--- step.java
+++ step.java
@@ -1,54 +1,15 @@
 public class step {
     public static void main(String args[]) {
-        String line1 = String.join("",
-                "  *****  ", "   ",
-                "  *****  ", "   ",
-                "  *****  ", "   ",
-                "  *****  ");
-
-        String line2 = String.join("",
-                " *     * ", "   ",
-                " *     * ", "   ",
-                " *     * ", "   ",
-                " *     * ");
-
-        String line3 = String.join("",
-                " *     * ", "   ",
-                " *     * ", "   ",
-                " *     * ", "   ",
-                " *       ");
-
-        String line4 = String.join("",
-                " *     * ", "   ",
-                " *     * ", "   ",
-                " *     * ", "   ",
-                "  *****  ");
-
-        String line5 = String.join("",
-                " *     * ", "   ",
-                " *     * ", "   ",
-                " *     * ", "   ",
-                "       * ");
-
-        String line6 = String.join("",
-                " *     * ", "   ",
-                " *     * ", "   ",
-                " *     * ", "   ",
-                " *     * ");
-
-        String line7 = String.join("",
-                "  *****  ", "   ",
-                "  *****  ", "   ",
-                "  *****  ", "   ",
-                "  *****  ");
-
-        System.out.println(line1);
-        System.out.println(line2);
-        System.out.println(line3);
-        System.out.println(line4);
-        System.out.println(line5);
-        System.out.println(line6);
-        System.out.println(line7);
-    }
-}
-    
+        for(int i = 1 ; i <= 5; i++ ) {
+            if(i == 1)
+                System.out.println(" *****   *****   *****   ***** ");
+            if(i == 2)
+                System.out.println("*     * *     *  *   *   *      "); 
+            if(i == 3)
+                System.out.println("*     * *     *  * * *   * * *");
+            if(i == 4)
+                System.out.println("*     * *     *  *           * ");
+            if(i == 5)
+                System.out.println(" *****   *****   *       ***** ");   
+        }     
+    }};
