f1e7e1c96c5e31d92fc94959991ecb3e0e2e63e5
kkakti2-stack <kakakikritangka2@gmail.com>
2026-03-04 19:14:52 +0530

    uc6

--- step.java
+++ step.java
@@ -1,53 +1,48 @@
 public class step {
     public static void main(String args[]) {
 
-        String[] lines = {
+        String[] o = getOPattern();
+        String[] p = getPPattern();
+        String[] s = getSPattern();
 
-                String.join(" ",
-                        " ***** ",
-                        " ***** ",
-                        " ***** ",
-                        " ***** "),
-
-                String.join(" ",
-                        " *   * ",
-                        " *   * ",
-                        " *   * ",
-                        " *     "),
-
-                String.join(" ",
-                        " *   * ",
-                        " *   * ",
-                        " ***** ",
-                        " ***** "),
-
-                String.join(" ",
-                        " *   * ",
-                        " *   * ",
-                        " *     ",
-                        "     * "),
-
-                String.join(" ",
-                        " *   * ",
-                        " *   * ",
-                        " *     ",
-                        "     * "),
-
-                String.join(" ",
-                        " *   * ",
-                        " *   * ",
-                        " *     ",
-                        " *   * "),
-
-                String.join(" ",
-                        " ***** ",
-                        " ***** ",
-                        " *     ",
-                        " ***** ")
-        };
-
-        for (String line : lines) {
-            System.out.println(line);
+        for (int i = 0; i < 7; i++) {
+            System.out.println(o[i] + " " + o[i] + " " + p[i] + " " + s[i]);
         }
+    }
+
+    public static String[] getOPattern() {
+        return new String[]{
+                " ***** ",
+                " *   * ",
+                " *   * ",
+                " *   * ",
+                " *   * ",
+                " *   * ",
+                " ***** "
+        };
+    }
+
+    public static String[] getPPattern() {
+        return new String[]{
+                " ***** ",
+                " *   * ",
+                " ***** ",
+                " *     ",
+                " *     ",
+                " *     ",
+                " *     "
+        };
+    }
+
+    public static String[] getSPattern() {
+        return new String[]{
+                " ***** ",
+                " *     ",
+                " ***** ",
+                "     * ",
+                "     * ",
+                " *   * ",
+                " ***** "
+        };
     }
 }
